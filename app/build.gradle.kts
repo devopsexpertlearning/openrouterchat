527460f212e6aa83d92d9aa2e62d1208e1db0a3b
DevOps Expert <info@devopsexpert.work.gd>
2025-03-19 23:57:17 +0530

    test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,7 +86,9 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.02.02"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3-window-size-class")
     implementation("androidx.compose.runtime:runtime-livedata")
 
     // Dependency Injection
